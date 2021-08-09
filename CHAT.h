ccf204113184aab9c9b7052acf1289c9d756f8f3
EvgeniiiV <84666289+EvgeniiiV@users.noreply.github.com>
2021-08-09 16:42:10 +0500

    Update CHAT.h

--- CHAT.h
+++ CHAT.h
@@ -54,4 +54,9 @@ template <typename T1, typename T2 > bool compare(T1* user, T2 u, T1* group, T2 
 template <typename T> bool compare_LP(T LP, T L, T P);//compares login and password
 template <typename T1, typename T2> T2 request(T1 rst, T2 a, T2 b, T2 first);//for registrarion
 template <typename T> T next_request();//case 5, 6
+inline size_t Factorial(size_t num);//factorial
+inline size_t Presence(size_t num);//to calculate max number of possible groups for one user based on the number of registered users
+
+
+
 
